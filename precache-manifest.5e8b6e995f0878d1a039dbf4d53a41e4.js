a8e438b3182da1d752e27f509848bede28a75252
Mahmoud Abu-seini <mahmoud.abuseini@luckytrip.co.uk>
2020-08-19 16:46:02 +0300

    Initial dist subtree commit

---
+++ precache-manifest.5e8b6e995f0878d1a039dbf4d53a41e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bc84eaa7ef2c8f29b6",
+    "url": "/vue/css/app.25b40837.css"
+  },
+  {
+    "revision": "3e67d360f145378e20a422bc4c3aaaa5",
+    "url": "/vue/index.html"
+  },
+  {
+    "revision": "90bc84eaa7ef2c8f29b6",
+    "url": "/vue/js/app.ad413660.js"
+  },
+  {
+    "revision": "3d28db3a6017e6ba6c3e",
+    "url": "/vue/js/chunk-vendors.c42bbc58.js"
+  },
+  {
+    "revision": "47b1f2c6c30ad438cd7b18b38eef9f44",
+    "url": "/vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue/robots.txt"
+  }
+]);
